67e210c80a196692dc9f7dbe20365858a44c724a
aizaz356 <aizaz3448@gmail.com>
2026-08-07 21:05:28 +0500

    add cin datatype

---
+++ Lecture 2/Cin-datatype.cpp
@@ -0,0 +1,14 @@
+// Cin(console input) datatype
+# include<iostream>
+using namespace std;
+
+int main() {
+    int val;
+
+    cout << "Enter value: ";
+
+    cin >> val;
+
+    cout << "Val was: " << val << endl;
+    return 0;
+}
