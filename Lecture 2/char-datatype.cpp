0ece81e1c0f70cd63f4f12597e986041825a173f
aizaz356 <aizaz3448@gmail.com>
2026-08-07 19:52:00 +0500

    add char datatype

---
+++ Lecture 2/char-datatype.cpp
@@ -0,0 +1,13 @@
+// char(character) data type example
+# include <iostream>
+using namespace std;
+
+int main() {
+    char C;                     // variable declaration
+
+    C = 'A';                    // ASCII value of A is 65
+    
+    //cout << (int)C << endl;  // type casting char to int
+        
+    return 0;
+}
